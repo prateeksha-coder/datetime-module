4c72084583432288e169a1902a7500aa279c7639
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-07-15 12:02:56 +0530

    initial commit

---
+++ a1.py
@@ -0,0 +1,20 @@
+import random
+
+import time
+
+
+def getRandomDate(starDate, endDate):
+    print("Printing random date between", starDate, "and", endDate)
+    randomGenerator = random.random()
+    dateFormat = '%m/%d/%Y'
+    starttime = time.mktime(time.strptime(starDate, dateFormat))
+    print(starttime)
+    endtime = time.mktime(time.strptime(endDate, dateFormat))
+    print(endtime)
+    randomTime = starttime + randomGenerator * (endtime - starttime)
+    print(randomTime)
+    randomDate = time.strftime(dateFormat, time.localtime(randomTime))
+    return randomDate
+
+
+print("Random Date = ", getRandomDate("1/1/2020", "12/12/2024"))
---
+++ a2.py
@@ -0,0 +1,7 @@
+import calendar
+obj=calendar.Calendar()
+print(obj.monthdatescalendar(2021,8),"\n\n")
+
+print(obj.monthdays2calendar(2021,8),"\n\n")
+print(obj.monthdayscalendar(2021,8),"\n\n")
+print(calendar.calendar(2025))

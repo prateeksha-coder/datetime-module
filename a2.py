a01b2e17fc38be325fd8aeae42fb29653b2788f0
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-08-23 22:16:43 +0530

    commit

--- a2.py
+++ a2.py
@@ -5,3 +5,7 @@ print(obj.monthdatescalendar(2021,8),"\n\n")
 print(obj.monthdays2calendar(2021,8),"\n\n")
 print(obj.monthdayscalendar(2021,8),"\n\n")
 print(calendar.calendar(2025))
+
+cal = calendar.Calendar()
+for day in cal.itermonthdays(2025, 8):
+    print(day)
